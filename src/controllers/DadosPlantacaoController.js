eb2fae130b0d66e77d6e0d0e5c8216c65a6d4a25
samuel-portela <samuelportela12@gmail.com>
2025-05-23 20:47:37 -0300

    fix: correção

---
+++ src/controllers/DadosPlantacaoController.js
@@ -0,0 +1,69 @@
+const DadosPlantacao = require('../models/DadosPlantacao');
+const bcrypt = require('bcryptjs');
+
+class DadosPlantacaoController{
+
+    async create(req,res){
+        let {nome, tipo_plantacao, token} = req.body;
+
+        let result = await DadosPlantacao.create(nome, tipo_plantacao, usuario_id);
+
+        result.status
+        ? res.status(200).json({sucess: true, message:"Usuário Cadastrado com Sucesso"})
+        : res.status(404).json({sucess: false, message: result.err})
+    }
+
+    async findAll(req,res){
+        let dadosUsuario = await DadosPlantacao.findAll();
+
+        dadosUsuario.status
+        ? res.status(200).json({sucess: true, values: dadosUsuario.values})
+        : res.status(404).json({sucess: false, message: dadosUsuario.err})
+    }
+
+    async findById(req,res){
+        if(isNaN(req.params.id) || !req.params.id){
+            return res.status(404).json({
+                message: 'Parametro ID obrigatório!'
+            });
+        }
+
+        let dadosUsuario = await DadosPlantacao.findById(req.params.id);
+
+        if (dadosUsuario.status == undefined){
+            res.status(406).json({sucess: false, message:"Não encontrado"});
+        }else if (!dadosUsuario.status){
+            res.status(404).json({sucess: false, message: dadosUsuario.err})
+        }else{
+            res.status(200).json({sucess: true, message:dadosUsuario.values})
+        }
+        
+    }
+
+    async editUser(req, res){
+        let id = req.params.id
+        
+        if(!id){
+            return res.status(404).json({sucess: false, message:'Parametro Inválido'})
+        }
+
+        let {nome,user,senha} = req.body
+
+        if(senha){
+            senha = bcrypt.hashSync(senha,bcrypt.genSaltSync(10))
+        }
+
+        if(!id){
+            return res.status(404).json({sucess: false, message:'Parametro Inválido'})
+        }
+            
+        let result = await Usuarios.update(id, nome, user,senha)
+        result.status 
+        ? res.status(200).json({sucess: result.status, message: result.message})
+        : res.status(406).json({sucess: result.status, message: result.err})
+        
+    }
+
+}
+
+module.exports = new DadosPlantacaoController();
---
+++ src/models/DadosPlantacao.js
@@ -0,0 +1,82 @@
+const knex = require('../data/connection');
+
+class DadosPlantacao{
+
+    async create(nome, tipo_plantacao, usuario_id){
+        try {
+            await knex.insert({
+                nome: nome,
+                tipo_plantacao: tipo_plantacao,
+                usuario_id: usuario_id
+            }).table('dados_plantacao');
+
+            return {status: true }
+        } catch (err) {
+            return {status: false, err: err}  
+        }
+    }
+
+    async findById(id){
+        try {
+            let user = await knex.select(['nome','tipo_plantacao','usuario_id']).where({id: id}).table('dados_plantacao');
+            return user.length > 0 ? {status: true, values: user } : {status: undefined, message: 'Usuário Não existe!'}
+        } catch (err) {
+            return {status: false, err: err}
+        }
+    }
+
+    async delete(id){
+        let user = await this.findById(id)
+
+        if (user.status){
+            try {
+                await knex.delete().where({id:id}).table('usuarios')
+                return {status: true, message:'Usuário Excluido com Sucesso'}
+            } catch (err) {
+                return {status: false, err: err}
+            }
+        }else{
+            return {status: false, err: 'Usuário não existe, portanto não pode ser deletado.'}
+        }
+    }
+
+    async updateSenha(email, senhaNova){
+        let usuario = await this.findByEmail(email)
+
+        if(usuario.status){
+            try {
+                await knex.update({senha: senhaNova}).where({id:usuario.values.id}).table('usuarios')
+                return ({status: true, message:'Usuário editado com sucesso!'})
+            } catch (err) {
+                return ({status: false, err: err})
+            }
+        }else{
+            return {status: false, err: 'Usuário não existe, portanto não pode ser editado.'}
+        }    
+    }
+
+    async findByEmail(email){
+        try {
+            let user = await knex.select(['id','nome','email','telefone','senha']).where({email:email}).table('usuarios')
+            return user.length > 0 ? {status: true, values: user[0]} : {status: false, err: undefined} 
+            
+        } catch (err) {
+            return {status: false, err: err}
+        }
+    }
+
+    async findByToken(token){
+        try {
+            let user = await knex.select(['id','nome','email','telefone','senha']).where({token_recuperacao:token}).table('usuarios')
+            return user.length > 0 ? {status: true, values: user[0]} : {status: false, err: undefined} 
+            
+        } catch (err) {
+            return {status: false, err: err}
+        }
+    }
+
+
+
+}
+
+module.exports = new DadosPlantacao();
